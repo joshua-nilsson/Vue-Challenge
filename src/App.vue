8e74761678e47170c4f6e62feed3832b84c00d22
Joshua Nilsson <27570195+joshua-nilsson@users.noreply.github.com>
2023-06-16 17:32:10 -0600

    Initial Commit - Vue 3 Composition API Refactor

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import AthleteData from "./assets/data.json"
+import AcademicFitReport from "./components/AcademicFitReport.vue"
+</script>
+
+<template>
+	<AcademicFitReport :athlete="AthleteData?.data[0]" />
+</template>
+
+<style scoped></style>
---
+++ src/components/AcademicFitReport.vue
@@ -0,0 +1,48 @@
+<script setup>
+defineProps({
+	athlete: {
+		type: Object,
+		required: true
+	}
+})
+</script>
+
+<template>
+	<main>
+		<div class="wrapper">
+			<img v-if="athlete?.profile_image" :src="athlete.profile_image" class="logo"
+				:alt="`${athlete?.name ? `${athlete.name}\'s` : 'Athlete\'s'} Profile Picture`" />
+		</div>
+		<div class="info">
+			<h2>{{ athlete?.name }}</h2>
+			<ul>
+				<li>
+					<label>Sport:</label>
+					<span>{{ athlete?.sport }}</span>
+				</li>
+				<li>
+					<label>Class:</label>
+					<span>{{ athlete?.grad_year }}</span>
+				</li>
+				<li>
+					<label>Club:</label>
+					<span>{{ athlete?.club?.name }}</span>
+				</li>
+				<li>
+					<label>High School:</label>
+					<span>{{ athlete?.high_school?.name }}</span>
+				</li>
+				<li>
+					<label>GPA:</label>
+					<span>{{ athlete?.gpa }}</span>
+				</li>
+				<li>
+					<label>Desired Major:</label>
+					<span>{{ athlete?.major }}</span>
+				</li>
+			</ul>
+		</div>
+	</main>
+</template>
+
+<style scoped></style>
